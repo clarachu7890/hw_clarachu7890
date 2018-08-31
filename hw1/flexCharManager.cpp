26f0ad9edada705bb408346e5c8e08f98e8caf39
Clara Chu <clarachu@usc.edu>
2018-08-31 11:38:34 -0700

    add all files

---
+++ hw1/flexCharManager.cpp
@@ -0,0 +1,16 @@
+#include "flexCharManager.h"
+
+flexCharManager::flexCharManager(){
+ 
+}
+
+flexCharManager::~flexCharManager(){
+}
+             
+char* flexCharManager::alloc_chars(int n){
+       return NULL;
+}
+
+void flexCharManager::free_chars(char* p){
+
+}         
---
+++ hw1/simpleCharManager.cpp
@@ -0,0 +1,18 @@
+#include <cstddef>
+#include "simpleCharManager.h"
+
+
+
+simpleCharManager::simpleCharManager(){
+
+}
+
+simpleCharManager::~simpleCharManager(){}
+             
+char* simpleCharManager::alloc_chars(int n){
+        return NULL;
+}
+
+void simpleCharManager::free_chars(char* p){
+
+}         
