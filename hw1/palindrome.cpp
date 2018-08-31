6f54efc5cd7024392750eecf769854350ac72141
Clara Chu <clarachu@usc.edu>
2018-08-31 10:54:58 -0700

    Update palindrome.cpp

--- hw1/palindrome.cpp
+++ hw1/palindrome.cpp
@@ -26,13 +26,31 @@ bool helper (stringstream &input, int len)
 	bool val = helper(input, len-2);
 
 	//make your changes only below this line.
+<<<<<<< HEAD
+	// the above function only indicates what letter is in the middle
+	char last;
+	input >> last;
+	if (prev==last) //try to check to see if the things left of the middle is equal to thing in the front equal to the 
+	{
+		return true;
+	}
+	else 
+	{
+		return false;
+	}
+=======
 	if (val) return true;
 	return false;
+>>>>>>> c96fb741505fc2a54b1074d9111bd4c147925529
 }
 
 //do not change the main function.
 int main (int argc, char* argv[])
+<<<<<<< HEAD
+{nhh  mj 
+=======
 {
+>>>>>>> c96fb741505fc2a54b1074d9111bd4c147925529
   if(argc < 2){
     cerr << "Please provide a string of characters." << endl;
     return 1;
