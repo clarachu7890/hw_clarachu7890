d340e6755ba15a22cfe1cb87d7840cd715d26158
Clara Chu <clarachu@usc.edu>
2018-09-02 21:55:13 -0700

    blanket stream

--- hw1/hw1q5.cpp
+++ hw1/hw1q5.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
 
+int main(int argc, char const *argv[])
+{
+   string word;
+
+   if(argc<1)// no file name
+   {
+      return 1;
+   }
+   ifstream wordFile (argv[1]);
+   if (!wordFile.is_open()) // file failed to open
+   {
+      return 1;
+   }
+   for (int i=0; i<10; i++)
+   {
+      wordFile >> word;
+      cout << word << endl;
+   }
+   
+   wordFile.close();
+
+   return 0;
+}
